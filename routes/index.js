e0e99f5dfcd440a6d1d1de44c258110d4b9f1e57
MinjaeLee <sochubert@gmail.com>
2020-10-27 22:19:23 +0900

    insert db info

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-east-02.cleardb.com',
+  user     : 'b533707a3ce3ab',
+  password : '204d04b4',
+  database : 'heroku_f30c74be296965b'
+});
+
+//DB에 정상 연결 되었는지 확인하는 부분.
+connection.connect(function(err){
+  if(err){
+    console.log("error connecting : " + err.stack);
+    return;
+  }
+  console.log('Success to DB connect!');
+})
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('../views/luxe/index', { title: 'Home' });

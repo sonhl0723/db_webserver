b8779a2c5b27a052158e89ff0888e0a3137ca6c8
1004gite <1004gite@naver.com>
2020-11-17 23:30:03 +0900

    base

---
+++ public/js/socket/complain_client.js
@@ -0,0 +1,21 @@
+$(
+    function (){
+
+        let socket = io();
+        //database 점속 후 있는거 받아오기
+
+        socket.on("addcomplain", function (data){
+            $("#x").append("<p>"+data+"</p>");
+        });
+
+
+        $("#myform").submit(function(e){
+           e.preventDefault();
+           var $complain = $("#complain");
+           console.log($complain.val());
+           socket.emit("addcomplain",{complaintext : $complain.val()});
+           $complain = "";
+
+        });
+    }
+)

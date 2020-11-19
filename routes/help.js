b7a7a6b87fe9a13d5d75d3eaafa30a5201853f2f
Minjae Lee <31719821+sochubert@users.noreply.github.com>
2020-11-19 17:51:13 +0900

    Update help.js

--- routes/help.js
+++ routes/help.js
@@ -2,14 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
     res.render('../views/chanwoong/help', { title: 'Help' , cust_info:null});
 });
 
-
-=======
-  res.render('../views/chanwoong/help', { title: 'Help' , cust_info:null});
-});
-
->>>>>>> 602193d5d80e5c84c2874364299e4007ac3e7a96
 module.exports = router;

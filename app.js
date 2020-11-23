614002695c41c76324ecc0bb7cf2968cca2f0305
MinjaeLee <sochubert@gmail.com>
2020-11-23 11:56:53 +0900

    loading reviews

--- app.js
+++ app.js
@@ -48,6 +48,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
+
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -56,5 +59,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/login.js
+++ routes/login.js
@@ -28,7 +28,7 @@ router.post('/',function (req,res) {
             console.log("로그인 성공!");
             user.customer_id = result[0]['CUSTOMER_ID'];
             user.customer_name = result[0]['KOR_LAST_NAME'] + result[0]['KOR_FIRST_NAME'];
-            console.log(user.customer_id);
+            console.log(user.customer_name);
 
             console.log("이거나오고 뒤에꺼가 안나온다 그거지??");
             res.cookie('customer_id', user.customer_id,{
@@ -37,7 +37,6 @@ router.post('/',function (req,res) {
             res.cookie('customer_name', user.customer_name,{
               maxAge : 60*60*1000,
             });
-            console.log("쿠키사용한다 싯팔" + req.cookies);
             res.redirect('/');
           }
         }
@@ -58,7 +57,7 @@ router.post('/',function (req,res) {
           console.log("로그인 성공!");
           employee.emp_id = rows[0]['login_id'];
           employee.emp_name = rows[0]['KOR_LAST_NAME'] + rows[0]['KOR_FIRST_NAME'];
-          console.log(employee.customer_id);
+          console.log(employee.emp_id);
 
           console.log("이거나오고 뒤에꺼가 안나온다 그거지??");
           res.cookie('employee_id', employee.emp_id,{
@@ -67,7 +66,6 @@ router.post('/',function (req,res) {
           res.cookie('employee_name', employee.emp_name,{
             maxAge : 60*60*1000,
           });
-          console.log("쿠키사용한다 싯팔" + req.cookies);
           res.redirect('/');
         }
       }
